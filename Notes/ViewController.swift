85eb02050ef6f70f8ce6c77fc805affa02805b62
Bogdan Mishura <fo_0x@mbp-fo-0x.dlink>
2019-05-07 20:59:52 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by fo_0x on 07.05.2019.
+//  Copyright © 2019 fo_0x. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
